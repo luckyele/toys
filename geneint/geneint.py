5db356e6250b87bbe70f3c0ad14bc67a8e60bb9b
luckyele <541913141@qq.com>
2018-02-07 21:27:16 +0800

    0208

--- geneint/geneint.py
+++ geneint/geneint.py
@@ -1,38 +1,56 @@
-from random import randint
+from random import randint, choice
+
+
 import cProfile
 
-# get a integer number K
+
 def read():
-	return int(input())
+    return int(input())
 
-# generate K integers
 def generate_int(k):
-	aint = []
+    aint = []
 
-	while True:
-		if len(aint) == k:
-			break
-		a = randint(1, k)
-		if a not in aint:
-			aint.append(a)
-			print(a)
-	return aint 	
+    while True:
+        if len(aint) == k:
+            break
+        a = randint(1, k)
+        if a not in aint:
+            aint.append(a)
+    return aint     
 
-# save
+def generate_int2(k):
+
+    aint = []
+    bint = []
+
+    for i in range(1, k+1):
+        bint.append(str(i))
+
+    while True:
+        if len(bint) == 0:
+            break
+        a = choice(bint)
+        bint.remove(a)      
+        aint.append(a)
+
+    return aint
+    
+
 def save(a):
-	f = open('data.txt', 'w')
-	for item in a:
-		f.write(str(item))
-		f.write('\n')
-	f.close()	
-	print("total write %d items to files."%len(a))
+    f = open('data.txt', 'w')
+    buf = ''
+    for item in a:
+        buf += str(item)+'\n'
+        
+    f.write(buf)
+    f.close()
+    print("total write %d items to files."%len(a))
 
-# main
+
 def main():
-	k = read()	
-	save(generate_int(k))
+    k = read()  
+    save(generate_int2(k))
+
 
 if __name__ == "__main__":
-	cProfile.run('main()')
-
-	
+    cProfile.run('main()')
