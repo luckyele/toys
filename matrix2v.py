94cc7340622378ecee486e9971fe3ec15fc82fe3
luckyele <541913141@qq.com>
2018-12-29 14:29:28 +0800

    set ~/.vimrc from vim.txt

--- matrix2v.py
+++ matrix2v.py
@@ -12,12 +12,13 @@ class Arry_map():
         self.map = [[0 for i in range(self.col)] for i in range(self.row)]
 
     def disp_arry_map(self):
-        for r in self.map:
-            for i in r:
-                if i == 1:
-                    print("\033[1;34;40m%d\033[40m"%(i),end='')
-                else:
-                    print("\033[0m%d"%(i),end='')
+		color = random.choice([34,35,36,37,38,39,40])
+		for r in self.map:
+		for i in r:
+			if i == 1:
+            	print("\033[1;34;40m%d\033[40m"%(color, i),end='')
+            else:
+                print("\033[0m%d"%(i),end='')
             print('\n', end='')	
        # print('\n')
 
@@ -45,7 +46,7 @@ class Arry_map():
         self.set_line(r1,c1,r2,c1)
 				
     def zoom_in_outside(self, r1, c1, r2, c2):
-				    pass
+    	pass
 				     
     def rand_rect(self):
         while True:
@@ -60,7 +61,7 @@ class Arry_map():
 	
 def test():
     arry_map = Arry_map()
-    arry_map.Arry_map_init(40, 40)
+    arry_map.Arry_map_init(40, 70)
     arry_map.disp_arry_map()
 			
     while True:
@@ -69,7 +70,6 @@ def test():
         arry_map.disp_arry_map()
         #arry_map.set_all_to_zero()
         time.sleep(.5)
-	
 
 if __name__ == "__main__":
     test()
