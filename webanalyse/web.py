975696655b60f54523ad81c0070572c02bb21069
luckyele <541913141@qq.com>
2019-05-03 08:47:59 -0400

    add

--- webanalyse/web.py
+++ webanalyse/web.py
@@ -2,54 +2,44 @@
 #coding:utf-8
 
 from bs4 import BeautifulSoup
-#coding:utf-8
-
 import requests
 import time
 
-host = 'http://www.ahwh.gov.cn'
-def _get_all_alink(url):
-	alinks = []
+
+host = 'http://ct.ah.gov.cn'
+INIT_LINKS = []
+ACCESSED_LINKS = []
+UNACCESSED_LINKS = []
+
+''' Get all <a> links in the current page.
+'''
+def get_all_alink(url, links):
 	r = requests.get(url)
-	soup = BeautifulSoup(r.text,'html.parser')
+	soup = BeautifulSoup(r.text, 'html.parser')
 	for link in soup.findAll('a'):
 		if 'href' in link.attrs:
 			t = link['href'].split('//')
 			if len(t) == 1 and t[0][0:1] == '/':
-				alinks.append(host+t[0])
-	return alinks
-
-def gen_graph(alinks):
-	url_graph = {}
-	i = 0
-	for link in alinks:
-		name = i 
-		url_graph[name] = link
-		i += 1
-	for url in url_graph.items():
-		print(url)
+				new_link = host + t[0]
+				if new_link not in links:
+					links.append(new_link)
 		
-def get_all_alinks(url):
-	links = []
-	links = _get_all_alink(url)
-	print('%d <a> link.'%len(links))
-	for link in links:
-		_links = _get_all_alink(link)
-		time.sleep(1)
-		if _links != None:
-			for l in _links:
-				if l not in links:
-					print("[%d Add] %s"%(len(links),l))
-					links.append(l)
-					if len(links) > 300:
-						return links
-	return links
+def test():
+	get_all_alink(host, INIT_LINKS)
+	UNACCESSED_LINKS = INIT_LINKS
+	if len(UNACCESSED_LINKS) > 0:
+		link = UNACCESSED_LINKS.pop(0)
+		ACCESSED_LINKS.append(link)
 
+def disp():
+	for link in ACCESSED_LINKS:
+		print(link)
+	print("")
+	
+	for link in UNACCESSED_LINKS:
+		print(link)
+	print("")
 
 if __name__ == "__main__":
-	url = "http://www.ahwh.gov.cn"
-	a = get_all_alinks(url)
-	
-
-				
-
+	test()
+	disp()
