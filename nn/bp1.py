56ffe0f8f37eb2016c97ecd2cba63631efc3193a
luckyele <pcsonline@163.com>
2019-10-10 16:50:28 +0800

    add

--- nn/bp1.py
+++ nn/bp1.py
@@ -53,19 +53,31 @@ def backprog(x, y):
     return (delta_w, delta_b)
 
 if __name__ == "__main__":
+    import csv
+    data = np.loadtxt('mess.csv',delimiter=',')
+    print(data.shape)
 
-    netword_sizes = [3,4,2]
+    #network_sizes = [3,4,2]
+    network_sizes = [51,4,4]
 
-    sizes = netword_sizes
+    sizes = network_sizes
     num_layers = len(sizes)
     biases = [np.random.randn(h, 1) for h in sizes[1:]]
     weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
-
+    print(num_layers)
     print(weights)
+    print('\n')
     print(biases)
+    print('\n')
+    print(data[0:50])
 
-    training_x = np.random.rand(3).reshape(3,1)
-    training_y = np.array([0,1]).reshape(2,1)
+   # training_x = np.random.rand(3).reshape(3,1)
+    training_x = np.array(data[0:50]).reshape[51,123]
+
+    #training_y = np.array([0,1]).reshape(2,1)
+
+    training_y = np.array(data[51]).reshape(12:4,1)
+
 
     print("training data x:{}, training y:{}\n".format(training_x,training_y))
 
