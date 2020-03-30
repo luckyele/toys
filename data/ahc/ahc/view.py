9789ff378ba8c76180d341ccb7d2211e37b08a4f
luckyele <pcsonline@163.com>
2020-03-30 22:56:11 +0800

    add

---
+++ data/ahc/ahc/view.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+import pandas as pd
+
+
+def read_data():
+    filename = "./20180521.xls"
+
+	pass    
+
+def hello(request):
+    return HttpResponse("Hello, world!")
